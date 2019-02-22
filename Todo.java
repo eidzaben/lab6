5b8087f60ef45e42a11f89ccd35b4513c9a9f54a
Eid Zaben <eid.s.zaben-1@ou.edu>
2019-02-22 09:19:52 -0600

    Todo added

---
+++ Todo.java
@@ -0,0 +1,27 @@
+public class Todo {/*
+Lab Section_________: 11
+Last Modified_______: 2019-02-22
+OU ID_______________: 113334522
+OU EMAIL____________: eid.s.zaben-1@ou.edu
+
+Tasks:
+1. Write the rectangle class
+2. Complete trapezoid, circle, and equilateral triangle classes
+3. Complete the shape sorter class
+4. Complete shapePerimeterComparator and shapeAreaComparator classes
+5. Complete Polygon and Shape classes
+
+Estimated Time (minutes):
+1. 30
+2. 30
+3. 25
+4. 30
+5. 20
+
+Actual Time (minutes):
+1. 
+2. 20
+3. 
+4. 
+5. 
+*/}
