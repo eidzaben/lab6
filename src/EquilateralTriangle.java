db5e24ed2975e32c3ebaac0421f4419a735d89eb
Eid Zaben <eid.s.zaben-1@ou.edu>
2019-02-22 08:59:04 -0600

    Equilateral triangle class complete

--- src/EquilateralTriangle.java
+++ src/EquilateralTriangle.java
@@ -9,7 +9,7 @@
 public class EquilateralTriangle extends Polygon
 {
     /**
-     * Lenght of the sides of the equilateral triangle.
+     * Length of the sides of the equilateral triangle.
      */
     private double sideLength;
 
@@ -21,7 +21,10 @@ public class EquilateralTriangle extends Polygon
 	 */
 	public EquilateralTriangle(String id, double sideLength)
 	{
-		// TODO: complete this...
+		super(id);
+		this.sideLength = sideLength;
+		
+		
 	}
 
 	/**
@@ -32,7 +35,8 @@ public class EquilateralTriangle extends Polygon
 	@Override
 	public double getArea()
 	{
-		// TODO: complete this...
+		
+		return sideLength * sideLength * Math.sqrt(3) / 4;
 	}
 
 	/**
@@ -43,6 +47,7 @@ public class EquilateralTriangle extends Polygon
 	@Override
 	public String getShapeType()
 	{
-		// TODO: complete this...
+		
+		return "EquilateralTriangle";
 	}
 }
