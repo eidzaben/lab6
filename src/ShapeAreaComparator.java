bb01f31148e96c8e7ae08f2ad05fadf0b5cc41c4
Eid Zaben <eid.s.zaben-1@ou.edu>
2019-02-22 12:14:15 -0600

    Shape Area Comparator class compple?

--- src/ShapeAreaComparator.java
+++ src/ShapeAreaComparator.java
@@ -33,6 +33,7 @@ public class ShapeAreaComparator implements Comparator<Shape>
 	public int compare(Shape s1, Shape s2)
 	{
 		// TODO: complete this...
+		return s1.compareTo(2);
 	}
 
 	/**
@@ -46,5 +47,11 @@ public class ShapeAreaComparator implements Comparator<Shape>
 	public boolean equals(Shape s1, Shape s2)
 	{
 		// TODO: complete this...
+		if (compare(s1,s2) == 0) {
+			return true;
+		} else {
+			return false;
+		}
+	
 	}
 }
